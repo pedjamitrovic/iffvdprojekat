cb68910253b9ebea6d8c093300a62b53039cd636
vladimir-siv <vladimir.sivcev@gmail.com>
2018-05-13 21:40:50 +0200

    Header/fixed/nav/footer templates

--- js/config.js
+++ js/config.js
@@ -1,3 +1,11 @@
 class Config { constructor() { throw new Error("Static class"); } };
 Config.mainPopupId = "popup";
 Config.alertPopupId = "alert-popup";
+
+$(document).ready(function()
+{
+	fixedHeader.load("templates/header-fixed.html");
+	header.load("templates/header-main.html");
+	navbar.load("templates/navbar.html");
+	footer.load("templates/footer.html");
+});
