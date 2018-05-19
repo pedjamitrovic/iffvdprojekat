13a3ede22fae31b0d3255b8613362bbb29f2dcf0
pedjamitrovic <pre11mit@yahoo.com>
2018-05-19 18:41:00 +0200

    Done 2/2 english translation

---
+++ js/config_eng.js
@@ -0,0 +1,27 @@
+class Config { constructor() { throw new Error("Static class"); } };
+
+// ================== CONFIG START ==================
+
+Config.mainPopupId = "popup";
+Config.alertPopupId = "alert-popup";
+
+// =================== CONFIG END ===================
+
+var win = $(window), doc = $(document);
+var body, wrapper, fixedHeader, header, navbar, content, footer;
+
+$(document).ready(function()
+{
+	body = $(document.body);
+	fixedHeader = $("body > div#wrapper > div#top-content-fixed > header#header-fixed");
+	wrapper = $("body > div#wrapper");
+	header = $("body > div#wrapper > header#header-main");
+	navbar = $("body > div#wrapper > nav#navbar-main");
+	content = $("body > div#wrapper > div#content");
+	footer = $("body > div#wrapper > footer#footer-main");
+	
+	fixedHeader.load("templates/header-fixed_eng.html");
+	header.load("templates/header-main_eng.html");
+	navbar.load("templates/navbar_eng.html");
+	footer.load("templates/footer_eng.html");
+});
